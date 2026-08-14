5b6d7e2670a1b087b2bdbc40b78fbad7e58ee223
CarMa90 <el.matteotti@gmail.com>
2026-08-14 11:18:32 -0700

    update login

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -115,19 +115,14 @@ module.exports.login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      console.log(`Ejecutando then ${user}`);
-      console.log(JWT_SECRET);
-      console.log(NODE_ENV);
       const token = jwt.sign(
         { _id: user._id },
-        (NODE_ENV = "production" ? JWT_SECRET : "dev-secret"),
+        NODE_ENV === "production" ? JWT_SECRET : "dev-secret",
+        { expiresIn: "7d" },
       );
-      console.log(`este es el token ${token}`);
       return res.status(200).send({ token });
     })
-    .catch((err) => {
-      console.log("estoy en el catch");
-      return next(err.message);
+    .catch(() => {
       return next(new UnauthorizedError("Verifique el email o contraseña"));
     });
 };
